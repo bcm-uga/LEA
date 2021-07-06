6e3cef4b8bbb0e3ae3860796d91b32ce709f43d5
gainc <gainc@timc-bcm-09p.local>
2021-07-06 15:18:36 +0200

    Add R2 version of genetic offset

--- R/lfmm2.R
+++ R/lfmm2.R
@@ -11,7 +11,7 @@ lfmm2 <- function(input,
                   env, 
                   K, 
                   lambda = 1e-5){
-  
+
 ## Check input response matrix 
 ## LEA  
     if (is.character(input)){
@@ -213,19 +213,22 @@ setMethod("lfmm2.test", "lfmm2Class",
 
 
 
-setGeneric("genetic.offset", function(object, 
-                                      input, 
+setGeneric("genetic.offset", function(input, 
                                       env, 
                                       new.env, 
                                       pop.labels,
-                                      pca = FALSE) matrix);
-setMethod("genetic.offset", "lfmm2Class",
-          function(object, 
-                   input, 
+                                      K,
+                                      pca = FALSE,
+                                      candidate.loci=NULL) matrix);
+setMethod("genetic.offset",
+          definition=function(input, 
                    env, 
                    new.env, 
                    pop.labels,
-                   pca){
+                   K,
+                   pca,
+                   candidate.loci){
+            
   ## Check input response matrix 
   ## LEA  
   if (is.character(input)){
@@ -286,6 +289,23 @@ setMethod("genetic.offset", "lfmm2Class",
       stop("The new environmental matrix contains NA.")
     }
   }
+         
+  ## Checking ploidy
+  cat("Checking ploidy.","\n")
+  if (max(lst.unique) > 2) stop("Only haploid or diploid genomes are allowed. For polypoids, perform haploidization (phasing) 
+                                and create copies of individual environmental data.")
+  
+  ### HAPLOIDIZATION: RANDOM PHASE
+  ### Random phase and duplication (n -> 2n)
+  
+  if (max(lst.unique) == 2)  cat("Random phase and duplication of samples: Create two haploid genomes from each individual genome.","\n")
+  if (max(lst.unique) == 2){
+    haplo <- haploidisation(Y, pop.labels, X, X.new)
+    Y <- haplo$haploid.genotype
+    pop.labels <- haplo$haploid.pop.labels
+    X <- haplo$haploid.env
+    X.new <- haplo$haploid.new.env
+  }
             
    
   d <-  ncol(X) #number of environmental variables
@@ -294,10 +314,7 @@ setMethod("genetic.offset", "lfmm2Class",
     stop("Number of columns in 'new.env' matrix is not equal to the number of columns in 'env' matrix")    
   }
   
-  ## Checking ploidy
-  cat("Checking ploidy.","\n")
-  if (max(lst.unique) > 2) stop("Only haploid or diploid genomes are allowed. For polypoids, perform haploidization (phasing) 
-                                and create copies of individual environmental data.")
+  
   
   n <-  nrow(X) #number of individuals
   n.new <-  nrow(X.new) #number of individuals
@@ -318,20 +335,15 @@ setMethod("genetic.offset", "lfmm2Class",
     stop("Number of pop labels not equal to the number of rows in the input matrix")    
   }
   
-  unique.labels <- unique(pop.labels)
+  unique.labels <- sort(unique(pop.labels))
   tab <- sapply(unique.labels, FUN = function(x) sum(pop.labels == x))
   if (min(tab) == 1){
     stop("Population samples in pop.labels must have more than one individual.")    
   }
   
-  ### HAPLOIDIZATION: RANDOM PHASE
-  ### Random phase and duplication (n -> 2n)
   
-  if (max(lst.unique) == 2)  cat("Random phase and duplication of samples: Create two haploid genomes from each individual genome.","\n")
   
-  ### TO DO 
-  ### Dedoubler Y, X et X.new, pop.labels, U (object@U)
-  ### n <- 2*n
+  object <- lfmm2(input=Y, env=X, K=K)
   
   ## compute effect sizes (B matrix)
   U <- object@U
@@ -356,24 +368,106 @@ setMethod("genetic.offset", "lfmm2Class",
   l.fit <- NULL
   l.pred <- NULL
   l.fitpred <- NULL
+  pop.labels.double <- c(pop.labels,pop.labels)
+  Y.fit.pred <- rbind(Y.fit, Y.pred)
+  offset.r2 <- c()
   
-  for (i in unique.labels){
-    l.fit <- c(l.fit, prcomp(Y.fit[pop.labels == i,], scale = TRUE)$sdev[1]^2)
-    l.pred <- c(l.pred, prcomp(Y.pred[pop.labels == i,], scale = TRUE)$sdev[1]^2)
-    l.fitpred <- c(l.fitpred, prcomp(rbind(Y.fit[pop.labels == i,], Y.pred[pop.labels == i,]), 
-                     scale = TRUE)$sdev[1]^2)
+  if (is.null(candidate.loci)){
+    candidate.loci <- seq(1,L)
   }
   
-  # 1 - F_LT = (1 - F_ST)/(1 - F_SL)
-  offset <- 1 - (L - l.fitpred)/(L - (l.pred + l.fit)/2)
+  for (i in unique.labels){
+    if (!pca){
+      genome_current_pop <- Y.fit.pred[pop.labels.double==i,]
+      
+      #### Preparing data for the regression
+      # Count number of individuals
+      nb.ind <- dim(genome_current_pop)[1]/2
+      # Labels for current and adapted population
+      pop.labels.current.futur <- c(rep(1, nb.ind), rep(2, nb.ind))
+      genome_current_pop <- scale(genome_current_pop)
+      
+      regression <- summary(lm(genome_current_pop ~ as.factor(as.numeric(pop.labels.current.futur))))
+      fst <- sapply(regression, FUN = function(smr) smr$r.squared)
+      offset.r2 <- c(offset.r2, mean(fst[candidate.loci]))
+    }else{
+      l.fit <- c(l.fit, prcomp(Y.fit[pop.labels == i, candidate.loci], scale = TRUE)$sdev[1]^2)
+      l.pred <- c(l.pred, prcomp(Y.pred[pop.labels == i, candidate.loci], scale = TRUE)$sdev[1]^2)
+      l.fitpred <- c(l.fitpred, prcomp(rbind(Y.fit[pop.labels == i, candidate.loci], Y.pred[pop.labels == i, candidate.loci]), 
+                     scale = TRUE)$sdev[1]^2)
+    }
+  }
   
-  offset <- rbind(offset, l.fit/L, l.pred/L, l.fitpred/L)
-  colnames(offset) <- unique.labels
-  rownames(offset) <- c("offset","F.fit","F.pred","F.fitpred")
+  if (!pca){
+    return(offset.r2)
+  }else{
+    # 1 - F_LT = (1 - F_ST)/(1 - F_SL)
+    offset <- 1 - (L - l.fitpred)/(L - (l.pred + l.fit)/2)
   
-  return(offset)
+    offset <- rbind(offset, l.fit/L, l.pred/L, l.fitpred/L)
+    colnames(offset) <- unique.labels
+    rownames(offset) <- c("offset","F.fit","F.pred","F.fitpred")
+  
+    return(offset)
+  }
 }
 )
+
+haploidisation <- function(genotype, pop.labels, env, new.env){
+  
+
+  #This function aims at generating a new haploid genotype matrix from a diploid genotype matrix
+  # It will also generate new matrices of env and new.env that will be twice the size of 
+  # the initial matrices.
+  
+  # store n and L values
+  n <- dim(genotype)[1]
+  L <- dim(genotype)[2]
+  
+  # Creation of a new matrix with 2*n individuals and L locus
+  haploid.genotype <- matrix(rep(0,(2*n*L)), nrow=2*n, ncol=L)
+  # New vector of pop.labels
+  haploid.pop.labels <- c()
+  # Haploid env matrices
+  haploid.env <- c()
+  haploid.new.env <- c()
+  
+  # For all diploid individuals, we will generate two diploid individual
+  for (i in seq(1,n)){
+    
+    current.individual <- genotype[i,]
+    
+    heterozygote.position <- current.individual == 1
+    homozygote.position <- current.individual == 2
+    random.vec <- rbinom(n=L, size=1, prob=0.5)
+    
+    # We will create two individuals following this logic : 
+    
+    # If at a given locus, diploid ind had 2 version of the derived allele
+    # Then at this given locus, the value for both haploid ind will be one
+    
+    # If at a given locus, diploid ind had 1 version of the derived allele
+    # Then we will affect randomly the 1 value to either the first or the second haploid ind
+    haploid.ind.1 <- homozygote.position + heterozygote.position*random.vec
+    haploid.ind.2 <- homozygote.position + (heterozygote.position - heterozygote.position*random.vec)
+    
+    haploid.genotype[(2*(i-1) + 1),] <- haploid.ind.1
+    haploid.genotype[2*i,] <- haploid.ind.2
+    
+    current.pop <- pop.labels[i]
+    haploid.pop.labels <- c(haploid.pop.labels, rep(current.pop, 2))
+    
+    current.env <- env[i,]
+    haploid.env <- rbind(haploid.env, current.env, current.env)
+    
+    current.new.env <- new.env[i,]
+    haploid.new.env <- rbind(haploid.new.env, current.new.env, current.new.env)
+  }
+  
+  return(list(haploid.genotype=haploid.genotype, haploid.pop.labels=haploid.pop.labels, haploid.env=haploid.env, haploid.new.env=haploid.new.env))
+}
+
+
 
 
 
